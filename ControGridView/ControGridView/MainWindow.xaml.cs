efa46d8b3d6fc93b8f22711c4861f3e10053754a
Jonathan-Agustin <99525287+Jonathan-Agustin@users.noreply.github.com>
2022-04-16 18:12:08 -0500

    Add files via upload

---
+++ ControGridView/ControGridView/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControGridView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Persona> personas = new ObservableCollection<Persona>();
+        private PersonaDataObjects personaDao = new PersonaDataObjects();
+        public MainWindow()
+        {
+            InitializeComponent();
+            personas = new ObservableCollection<Persona>(personaDao.listar());
+            listViewAgenda.ItemsSource = personas;
+        }
+    }
+}
---
+++ ControGridView/ControGridView/PersonaDataObjects.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControGridView
+{
+
+    internal class PersonaDataObjects
+    {
+        private List<Persona> listaPersonas = new List<Persona>();
+
+        public PersonaDataObjects()
+        {
+            Persona p1 = new Persona { Nombre = "Javier", Email = "javier@email.com"
+            , Genero = "hombre", Telefono = "2228889993", ImagenGenero = "Hombre.png"};
+            Persona p2 = new Persona { Nombre = "Rebecca", Email = "rebecca@email.com"
+            , Genero = "mujer", Telefono = "2226655447", ImagenGenero = "Mujer.png"};
+            listaPersonas.Add(p1);
+            listaPersonas.Add(p2);
+        }
+
+        public List<Persona> listar()
+        {
+            return listaPersonas;
+        }
+    }
+}
